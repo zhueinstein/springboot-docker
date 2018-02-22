97ce8631b238e9d0d6672fa68643591cb059e9cf
zhuweifeng <675748324@qq.com>
2018-02-22 14:36:24 +0800

    feat: 新增加Dockerfile文件

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+}
